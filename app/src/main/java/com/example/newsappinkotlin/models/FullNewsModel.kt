efad97512964c4ebba5b05cd04b18357efd8052c
Kareem Amr Younes <official.kyounes@gmail.com>
2020-10-06 08:20:23 +0200

    created the fullNewsModel class in the models package

---
+++ app/src/main/java/com/example/newsappinkotlin/models/FullNewsModel.kt
@@ -0,0 +1,18 @@
+package com.example.newsappinkotlin.models
+
+import com.google.gson.annotations.SerializedName
+
+data class FullNewsModel (
+    @SerializedName("name") var headLineSource: Source,
+    @SerializedName("author") var newsAuthor: String,
+    @SerializedName("description") var newsDescription: String,
+    @SerializedName("title") var headLineTitle: String,
+    @SerializedName("urlToImage") var headLineThumbNail: String,
+    @SerializedName("publishedAt") var headLinePublish: String,
+    @SerializedName("content") var newContent: String
+)
+
+class Source(
+    val id: String?,
+    val name: String
+)
