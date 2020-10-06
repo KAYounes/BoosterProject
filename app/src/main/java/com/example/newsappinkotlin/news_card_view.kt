f0e9f7df9bd719b75257bc73df558bbdc642e068
Kareem Amr Younes <official.kyounes@gmail.com>
2020-10-06 08:04:14 +0200

    added headlines + newssource colors

---
+++ app/src/main/java/com/example/newsappinkotlin/news_card_view.kt
@@ -0,0 +1,11 @@
+package com.example.newsappinkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class news_card_view : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_card_view)
+    }
+}
